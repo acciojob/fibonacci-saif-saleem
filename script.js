a4aa9281b477c159471864331027b1c74827dccf
saif-saleem <133539527+saif-saleem@users.noreply.github.com>
2023-09-08 21:08:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+	if(num==0)
+		return 0;
+	if (num==1)
+		return 1;
+	let s=fibonacci(num-1);
+	let p=fibonacci(num-2);
+	return p+q;
 }
 
 module.exports = fibonacci;
